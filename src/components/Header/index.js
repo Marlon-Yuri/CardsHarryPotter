6f740a205a7470eed0d0be93f5e2046989fbc34c
Marlon-Yuri <marlonyurimi@gmail.com>
2021-12-17 19:51:29 -0300

    update

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -19,7 +19,7 @@ export function Header() {
        <img src ="https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2021/11/WhatsApp-Image-2021-11-16-at-14.52.17.jpeg?w=1200&h=675&crop=1" alt="" width="300"/>   
       <S.List>
         <li>
-      <StyledLink to="/Sobre">J. K.Rowling</StyledLink>
+      <StyledLink to="/Sobre">The Author</StyledLink>
       </li>      
       <li>
       <StyledLink to="/Casas"> Casas </StyledLink>   
